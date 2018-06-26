9fd0dbaa33f050927dab39d616c57e05fad71ebe
toya0129 <citu15059@g.nihon-u.ac.jp>
2018-06-26 21:37:14 +0900

    first commit

---
+++ Assets/script/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+	public colorChange cc;
+
+	public GameObject ar;
+	public static bool trigger;
+	private bool cct;
+
+	public Text tx;
+	private int ran;
+
+	private string start;
+	private string change;
+
+	// Use this for initialization
+	void Start () {
+		cct = true;
+		trigger = false;
+		start = "カメラに\r\n写真をかざす";
+		change = "オブジェクトをタッチ";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ar.SetActive (trigger);
+
+		if (trigger == true) {
+			tx.text = change;
+			if (cct == true) {
+				cc.ct = true;
+				cct = false;
+			}
+		} else {
+			tx.text = start;
+			cct = true;
+		}
+	}
+
+
+
+	public void VideoPlay(){
+		Debug.Log ("再生開始");
+		Handheld.PlayFullScreenMovie ("FloorPadPV.mp4", new Color (0, 0, 0));
+	}
+		
+}
---
+++ Assets/script/colorChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour {
+
+	private Renderer arRen;
+	public Material[] mate;
+	private int ran;
+
+	public bool ct;
+
+	// Use this for initialization
+	void Start () {
+		arRen = this.GetComponent<Renderer> ();
+		ct = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ct == true) {
+			change ();
+		}
+	}
+
+	public void change (){
+		Debug.Log (ran);
+		ran = Random.Range (0, 5);
+		arRen.material = mate [ran];
+		ct = false;
+	}
+}
